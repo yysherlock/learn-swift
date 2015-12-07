87a11a15d39dca9dd81dee2fd930346dc1497ff6
Zhiyi Luo <jessieluo1991@gmail.com>
2015-12-07 13:05:18 +0800

    basics

---
+++ Basics.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let constantString = "Hi" // define a constant
+//constantString = "xixi" // error because you can change a constant 
+
+var str = "Hello, playground"
+// str = nil // error: nil is not a string
+var strq : String? = "Hello World!" // Optional type
+
+strq
+
+print(strq)
+
